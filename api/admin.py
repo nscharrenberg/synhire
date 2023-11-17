7cf68bab2dc9b986ef1ad5a53cca7c09e8ea3736
Noah Scharrenberg <nscharrenberg@hotmail.com>
2023-11-17 14:22:48 +0100

    add django admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,83 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Applicant, Company, Skill, Language, Certificate, Vacancy, EducationalInstitute
+
+
+class ApplicantSkillInline(admin.TabularInline):
+    model = Applicant.skills.through
+    extra = 0
+
+
+class ApplicantLanguageInline(admin.TabularInline):
+    model = Applicant.languages.through
+    extra = 0
+
+
+class ApplicantCertificateInline(admin.TabularInline):
+    model = Applicant.certificates.through
+    extra = 0
+
+
+class ApplicantEducationInline(admin.TabularInline):
+    model = Applicant.education.through
+    extra = 0
+
+
+class ApplicantWorkExperienceInline(admin.TabularInline):
+    model = Applicant.external_experience.through
+    extra = 0
+
+
+class VacancyApplicantInline(admin.TabularInline):
+    model = Vacancy.applications.through
+    extra = 0
+
+
+@admin.register(Applicant)
+class ApplicantModel(admin.ModelAdmin):
+    inlines = [
+        ApplicantWorkExperienceInline,
+        ApplicantEducationInline,
+        ApplicantSkillInline,
+        ApplicantLanguageInline,
+        ApplicantCertificateInline,
+        VacancyApplicantInline
+    ]
+
+
+class VacancySkillInline(admin.TabularInline):
+    model = Vacancy.skills.through
+    extra = 0
+
+
+class VacancyLanguageInline(admin.TabularInline):
+    model = Vacancy.languages.through
+    extra = 0
+
+
+class VacancyCertificateInline(admin.TabularInline):
+    model = Vacancy.certificates.through
+    extra = 0
+
+
+@admin.register(Vacancy)
+class VacancyModel(admin.ModelAdmin):
+    inlines = [
+        VacancySkillInline,
+        VacancyLanguageInline,
+        VacancyCertificateInline,
+        VacancyApplicantInline
+    ]
+
+
+@admin.register(EducationalInstitute)
+class EducationalInstituteModel(admin.ModelAdmin):
+    inlines = [
+        ApplicantEducationInline
+    ]
+
+
+admin.site.register(Company)
+admin.site.register(Skill)
+admin.site.register(Language)
+admin.site.register(Certificate)
--- api/models.py
+++ api/models.py
@@ -8,33 +8,48 @@ class EducationalInstitute(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name} - {self.location}"
+
 
 class Language(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     applicants = models.ManyToManyField(to='Applicant', through='ApplicantLanguage')
     vacancies = models.ManyToManyField(to='Vacancy', through='VacancyLanguage')
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Skill(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     applicants = models.ManyToManyField(to='Applicant', through='ApplicantSkill')
     vacancies = models.ManyToManyField(to='Vacancy', through='VacancySkill')
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Certificate(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     applicants = models.ManyToManyField(to='Applicant', through='ApplicantCertificate')
     vacancies = models.ManyToManyField(to='Vacancy', through='VacancyCertificate')
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
     size = models.IntegerField(validators=[MinValueValidator(0)])
-    revenue = models.DecimalField(null=True, max_digits=18, decimal_places=2, validators=[MinValueValidator(0.00)])
+    revenue = models.DecimalField(null=True, blank=True, max_digits=18, decimal_places=2, validators=[MinValueValidator(0.00)])
     industry = models.CharField(max_length=255)
-    rating = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    description = models.TextField(null=True)
+    rating = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    description = models.TextField(null=True, blank=True)
     ex_employees = models.ManyToManyField(to='Applicant', through='WorkExperience')
+
+    def __str__(self):
+        return f"{self.name} ({self.industry} - {self.size})"
 
 
 class Applicant(models.Model):
@@ -44,20 +59,27 @@ class Applicant(models.Model):
     skills = models.ManyToManyField(to='Skill', through='ApplicantSkill')
     external_experience = models.ManyToManyField(to='Company', through='WorkExperience')
     certificates = models.ManyToManyField(to='Certificate', through='ApplicantCertificate')
+    vacancies = models.ManyToManyField(to='Vacancy', through='VacancyApplication')
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Vacancy(models.Model):
     title = models.CharField(max_length=255)
     company = models.ForeignKey(to='Company', on_delete=models.CASCADE)
     location = models.CharField(max_length=255)
-    salary = models.DecimalField(null=True, max_digits=10, decimal_places=2, validators=[MinValueValidator(0.00)])
-    desired_degree = models.TextField(null=True)
-    required_degree = models.TextField(null=True)
+    salary = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=2, validators=[MinValueValidator(0.00)])
+    desired_degree = models.TextField(null=True, blank=True)
+    required_degree = models.TextField(null=True, blank=True)
     role = models.TextField()
     skills = models.ManyToManyField(to='Skill', through='VacancySkill')
     languages = models.ManyToManyField(to='Language', through='VacancyLanguage')
     certificates = models.ManyToManyField(to='Certificate', through='VacancyCertificate')
     applications = models.ManyToManyField(to='Applicant', through='VacancyApplication')
+
+    def __str__(self):
+        return f"{self.title} ({self.company}|{self.location}|{self.role}|{self.salary})"
 
 
 class ApplicantLanguage(models.Model):
@@ -70,8 +92,11 @@ class ApplicantEducation(models.Model):
     applicant = models.ForeignKey(to='Applicant', on_delete=models.CASCADE)
     institute = models.ForeignKey(to='EducationalInstitute', on_delete=models.CASCADE)
     started_at = models.DateField()
-    graduated_at = models.DateField(null=True)
+    graduated_at = models.DateField(null=True, blank=True)
     degree = models.TextField()
+
+    def __str__(self):
+        return f"{self.applicant} studied {self.degree} at {self.institute}"
 
 
 class ApplicantSkill(models.Model):
@@ -79,36 +104,51 @@ class ApplicantSkill(models.Model):
     skill = models.ForeignKey(to='Skill', on_delete=models.CASCADE)
     proficiency = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
 
+    def __str__(self):
+        return f"{self.applicant} masters {self.skill}"
+
 
 class ApplicantCertificate(models.Model):
     applicant = models.ForeignKey(to='Applicant', on_delete=models.CASCADE)
     certificate = models.ForeignKey(to='Certificate', on_delete=models.CASCADE)
-    received_at = models.DateField(null=True)
-    expired_at = models.DateField(null=True)
+    received_at = models.DateField(null=True, blank=True)
+    expired_at = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.applicant} received {self.certificate}"
 
 
 class WorkExperience(models.Model):
     applicant = models.ForeignKey(to='Applicant', on_delete=models.CASCADE)
     company = models.ForeignKey(to='Company', on_delete=models.CASCADE)
     hired_at = models.DateField()
-    left_at = models.DateField(null=True)
-    hired_reason = models.TextField(null=True)
-    left_reason = models.TextField(null=True)
+    left_at = models.DateField(null=True, blank=True)
+    hired_reason = models.TextField(null=True, blank=True)
+    left_reason = models.TextField(null=True, blank=True)
     role = models.TextField()
+
+    def __str__(self):
+        return f"{self.applicant} worked as a {self.role} at {self.company}"
 
 
 class VacancySkill(models.Model):
     vacancy = models.ForeignKey(to='Vacancy', on_delete=models.CASCADE)
     skill = models.ForeignKey(to='Skill', on_delete=models.CASCADE)
-    desired_proficiency = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    required_proficiency = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    desired_proficiency = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    required_proficiency = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.vacancy} expects the applicant to have the skill: {self.skill}"
 
 
 class VacancyLanguage(models.Model):
     vacancy = models.ForeignKey(to='Vacancy', on_delete=models.CASCADE)
     language = models.ForeignKey(to='Language', on_delete=models.CASCADE)
-    desired_proficiency = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    required_proficiency = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    desired_proficiency = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    required_proficiency = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.vacancy} expects the applicant to have the language: {self.language}"
 
 
 class VacancyCertificate(models.Model):
@@ -116,11 +156,17 @@ class VacancyCertificate(models.Model):
     certificate = models.ForeignKey(to='Certificate', on_delete=models.CASCADE)
     required = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.vacancy} expects the applicant to have certificate: {self.certificate}"
+
 
 class VacancyApplication(models.Model):
     vacancy = models.ForeignKey(to='Vacancy', on_delete=models.CASCADE)
     applicant = models.ForeignKey(to='Applicant', on_delete=models.CASCADE)
-    hired_at = models.DateField()
-    left_at = models.DateField(null=True)
-    hired_reason = models.TextField(null=True)
-    left_reason = models.TextField(null=True)
+    hired_at = models.DateField(null=True, blank=True)
+    left_at = models.DateField(null=True, blank=True)
+    hired_reason = models.TextField(null=True, blank=True)
+    left_reason = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.applicant} applied to {self.vacancy}"
